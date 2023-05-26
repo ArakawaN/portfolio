6b3fc4b97339b8fa0e22366f1a7ece1837a7f5a1
ArakawaN <arkw1155@gmail.com>
2023-05-27 00:24:13 +0900

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Layout = () => {
+  return (
+    <div>
+      <Head>
+        <nav class="w-full">
+          <div class="flex justify-between w-full px-16 py-8">
+            <p>Arakawa Naoya</p>
+          </div>
+        </nav>
+      </Head>
+    </div>
+  );
+};
+
+export default Layout;
